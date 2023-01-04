0bbc134c3035f978f37591ab34e4dd4f48778651
JackwillJudge <jack.w.judge@gmail.com>
2023-01-04 18:47:37 +1300

    Updates

---
+++ precache-manifest.c4a60c24549f32b0a63d2329e526543a.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cb9dd5c41cd2e3945f2",
+    "url": "/OceanCovers/static/css/main.bd2ca1c6.chunk.css"
+  },
+  {
+    "revision": "9cb9dd5c41cd2e3945f2",
+    "url": "/OceanCovers/static/js/main.9cb9dd5c.chunk.js"
+  },
+  {
+    "revision": "c2f76bae2d092c75433e",
+    "url": "/OceanCovers/static/js/1.c2f76bae.chunk.js"
+  },
+  {
+    "revision": "1ea941efb81e3e0f4d78",
+    "url": "/OceanCovers/static/css/2.3d515b78.chunk.css"
+  },
+  {
+    "revision": "1ea941efb81e3e0f4d78",
+    "url": "/OceanCovers/static/js/2.1ea941ef.chunk.js"
+  },
+  {
+    "revision": "6e911cda6cd960045805",
+    "url": "/OceanCovers/static/js/runtime~main.6e911cda.js"
+  },
+  {
+    "revision": "0e8cc8248564512c3948fc9295e093f6",
+    "url": "/OceanCovers/static/media/Hero2.0e8cc824.jpg"
+  },
+  {
+    "revision": "6d9c3a2ea946b8e4c03c7a4328460c55",
+    "url": "/OceanCovers/static/media/galleryCoversFront1.6d9c3a2e.jpg"
+  },
+  {
+    "revision": "9c6976c45743f4ca4dcdf2b6c06f47da",
+    "url": "/OceanCovers/static/media/galleryCoversFront2.9c6976c4.jpg"
+  },
+  {
+    "revision": "2814d08f3f4cb12ed32ccc235c9d67f8",
+    "url": "/OceanCovers/static/media/galleryCovers1.2814d08f.jpg"
+  },
+  {
+    "revision": "4395a7952a34000350d8712ac8a2ac3b",
+    "url": "/OceanCovers/static/media/galleryCovers2.4395a795.jpg"
+  },
+  {
+    "revision": "096462f5ba573a3d53842aaa4d685001",
+    "url": "/OceanCovers/static/media/galleryCovers3.096462f5.jpg"
+  },
+  {
+    "revision": "647ccd23b565a3a1039063096f7423e7",
+    "url": "/OceanCovers/static/media/galleryCovers4.647ccd23.jpg"
+  },
+  {
+    "revision": "3e953a56c566b49134c206dcfdd5e6e5",
+    "url": "/OceanCovers/static/media/galleryCovers5.3e953a56.jpg"
+  },
+  {
+    "revision": "456365c775f6887311cfead9e259228a",
+    "url": "/OceanCovers/static/media/galleryCovers6.456365c7.jpg"
+  },
+  {
+    "revision": "9884953b4ca4d2e1b0e3555e71eaf56e",
+    "url": "/OceanCovers/static/media/galleryCovers7.9884953b.jpg"
+  },
+  {
+    "revision": "84afc4f7a763f22cb31a9b770bd38c9b",
+    "url": "/OceanCovers/static/media/galleryCovers8.84afc4f7.jpg"
+  },
+  {
+    "revision": "e29eb595123345cd76df0efd445dbd3b",
+    "url": "/OceanCovers/static/media/galleryCovers9.e29eb595.jpg"
+  },
+  {
+    "revision": "567d875bcef8c212bcd8c20888d0ea59",
+    "url": "/OceanCovers/static/media/galleryCovers10.567d875b.jpg"
+  },
+  {
+    "revision": "faa727f8fbd4ce0c4eb28a55dd624553",
+    "url": "/OceanCovers/static/media/galleryCovers11.faa727f8.jpg"
+  },
+  {
+    "revision": "36b4031c10da1b69fe4536d14e4354e7",
+    "url": "/OceanCovers/static/media/galleryCovers12.36b4031c.jpg"
+  },
+  {
+    "revision": "18527e480ce5ebcf4c849a98d2661fdf",
+    "url": "/OceanCovers/static/media/galleryCovers13.18527e48.jpg"
+  },
+  {
+    "revision": "3ba421bb5dd3db00622ab8e857c1a780",
+    "url": "/OceanCovers/static/media/galleryCovers14.3ba421bb.jpg"
+  },
+  {
+    "revision": "d1dc77889bca08a070ee57d849e3e026",
+    "url": "/OceanCovers/static/media/galleryCovers15.d1dc7788.jpg"
+  },
+  {
+    "revision": "8204395c02863a6a8e141850f090b487",
+    "url": "/OceanCovers/static/media/galleryCovers16.8204395c.jpg"
+  },
+  {
+    "revision": "cebe303a9bfaecfba7d75e7374c7e3a4",
+    "url": "/OceanCovers/static/media/galleryCovers17.cebe303a.jpg"
+  },
+  {
+    "revision": "c2f78f7ee2ca3f3eb4d5e26f1e0d6168",
+    "url": "/OceanCovers/static/media/galleryCovers18.c2f78f7e.jpg"
+  },
+  {
+    "revision": "982b7a96d29a96bbe8f9e0c8165edce1",
+    "url": "/OceanCovers/static/media/galleryCovers19.982b7a96.jpg"
+  },
+  {
+    "revision": "46bfd12306613f6e7f0441d6dff2e3f1",
+    "url": "/OceanCovers/static/media/galleryCovers20.46bfd123.jpg"
+  },
+  {
+    "revision": "ae0d753a52f70ab275355359969cfcab",
+    "url": "/OceanCovers/static/media/galleryCovers21.ae0d753a.jpg"
+  },
+  {
+    "revision": "480805a061220faf389aa0eba08d8636",
+    "url": "/OceanCovers/static/media/galleryCovers22.480805a0.jpg"
+  },
+  {
+    "revision": "b2c48be4b919c2a08cad547cb261d3bf",
+    "url": "/OceanCovers/static/media/galleryCovers23.b2c48be4.jpg"
+  },
+  {
+    "revision": "037a7867270d4e0c6a4ecce65901d648",
+    "url": "/OceanCovers/static/media/galleryCovers24.037a7867.jpg"
+  },
+  {
+    "revision": "7aa70dd2bb0fc68c44a2b593a2ebb9eb",
+    "url": "/OceanCovers/static/media/galleryCovers25.7aa70dd2.jpg"
+  },
+  {
+    "revision": "1b1bfb9455993f478be074ca7edd7177",
+    "url": "/OceanCovers/static/media/galleryCovers26.1b1bfb94.jpg"
+  },
+  {
+    "revision": "8cd1890745e6c2fc1ca7f1e79a073fd5",
+    "url": "/OceanCovers/static/media/galleryCovers27.8cd18907.jpg"
+  },
+  {
+    "revision": "11fe2959e1fa3d90b2017f7d3ca9961c",
+    "url": "/OceanCovers/static/media/galleryCovers28.11fe2959.jpg"
+  },
+  {
+    "revision": "a558fedd8b57f5373dbd2835faea5ed3",
+    "url": "/OceanCovers/static/media/galleryCovers29.a558fedd.jpg"
+  },
+  {
+    "revision": "87665f5450eb550c9a32cf9f7b8a128e",
+    "url": "/OceanCovers/static/media/galleryCovers30.87665f54.jpg"
+  },
+  {
+    "revision": "0f76446c3983deff5c610ac5d0f74c0e",
+    "url": "/OceanCovers/static/media/galleryUpholstery1.0f76446c.jpg"
+  },
+  {
+    "revision": "3c5eac0edddf99280dc31e7bec6ee99b",
+    "url": "/OceanCovers/static/media/galleryUpholstery2.3c5eac0e.jpg"
+  },
+  {
+    "revision": "86a94565c950128551adf04009d5f553",
+    "url": "/OceanCovers/static/media/galleryUpholstery3.86a94565.jpg"
+  },
+  {
+    "revision": "b3372f1a2b70fe8128fcd7d4b4cb3367",
+    "url": "/OceanCovers/static/media/galleryUpholstery4.b3372f1a.jpg"
+  },
+  {
+    "revision": "7affdf03bb28e966a26d549ac3a5002f",
+    "url": "/OceanCovers/static/media/galleryUpholstery5.7affdf03.jpg"
+  },
+  {
+    "revision": "422e44be027bf91abf834e6ce9b0ff55",
+    "url": "/OceanCovers/static/media/galleryUpholstery6.422e44be.jpg"
+  },
+  {
+    "revision": "e874098356cd80c18dad3d98f0376946",
+    "url": "/OceanCovers/static/media/galleryUpholstery7.e8740983.jpg"
+  },
+  {
+    "revision": "cd0ee1444439054e9e7e6e4122614751",
+    "url": "/OceanCovers/static/media/galleryUpholstery8.cd0ee144.jpg"
+  },
+  {
+    "revision": "89bb0e37cf5d295361c691fba1d75644",
+    "url": "/OceanCovers/static/media/galleryUpholstery9.89bb0e37.jpg"
+  },
+  {
+    "revision": "c5208d37286a2585ea926959bc3252b8",
+    "url": "/OceanCovers/static/media/galleryUpholstery10.c5208d37.jpg"
+  },
+  {
+    "revision": "4f39191ee432b0566d969695b006fcc2",
+    "url": "/OceanCovers/static/media/galleryUpholstery11.4f39191e.jpg"
+  },
+  {
+    "revision": "34e8e0e45b89af2b20e9ca53e41e643e",
+    "url": "/OceanCovers/static/media/galleryUpholstery12.34e8e0e4.jpg"
+  },
+  {
+    "revision": "3e953a56c566b49134c206dcfdd5e6e5",
+    "url": "/OceanCovers/static/media/galleryUpholstery13.3e953a56.jpg"
+  },
+  {
+    "revision": "ae6aca0bf3390dec39938863fc264bfe",
+    "url": "/OceanCovers/static/media/galleryUpholstery14.ae6aca0b.jpg"
+  },
+  {
+    "revision": "456365c775f6887311cfead9e259228a",
+    "url": "/OceanCovers/static/media/galleryUpholstery15.456365c7.jpg"
+  },
+  {
+    "revision": "ff1fc8dfe0aa205666bdfd2a86c48577",
+    "url": "/OceanCovers/static/media/galleryUpholstery16.ff1fc8df.jpg"
+  },
+  {
+    "revision": "88e93266c1b76774227c450753ab48de",
+    "url": "/OceanCovers/static/media/galleryUpholstery17.88e93266.jpg"
+  },
+  {
+    "revision": "adc0dd49779d9af13450540b1bc192c5",
+    "url": "/OceanCovers/static/media/galleryUpholstery18.adc0dd49.jpg"
+  },
+  {
+    "revision": "34c5d4ca44a4cb8a42a70077cd914cdb",
+    "url": "/OceanCovers/static/media/galleryUpholstery20.34c5d4ca.jpg"
+  },
+  {
+    "revision": "091b9952207acb4bf663813efebf19bb",
+    "url": "/OceanCovers/static/media/galleryUpholstery21.091b9952.jpg"
+  },
+  {
+    "revision": "4ad2e5174c94eba6e77f6466249cbc22",
+    "url": "/OceanCovers/static/media/galleryUpholstery22.4ad2e517.jpg"
+  },
+  {
+    "revision": "48d86db6d0fc2194feeb50a254ee7a4f",
+    "url": "/OceanCovers/static/media/galleryUpholstery23.48d86db6.jpg"
+  },
+  {
+    "revision": "fe33e02105185951f5ca0888dc459dbd",
+    "url": "/OceanCovers/static/media/other1.fe33e021.jpg"
+  },
+  {
+    "revision": "5f99e6f66dd8897a3c2da89d4d0f8441",
+    "url": "/OceanCovers/static/media/other2.5f99e6f6.jpg"
+  },
+  {
+    "revision": "f2742e6d5ea28276055304184ea5ad01",
+    "url": "/OceanCovers/static/media/other3.f2742e6d.jpg"
+  },
+  {
+    "revision": "25b16d38d967c62fa2ddad3820c8f422",
+    "url": "/OceanCovers/static/media/other4.25b16d38.jpg"
+  },
+  {
+    "revision": "bd3f32af35e109e01b9a9804470fd756",
+    "url": "/OceanCovers/static/media/logo.bd3f32af.png"
+  },
+  {
+    "revision": "83a7a611311c380cee53a058efac5df0",
+    "url": "/OceanCovers/static/media/Hero1.83a7a611.jpg"
+  },
+  {
+    "revision": "180e168044bbda43524fce2bffce208c",
+    "url": "/OceanCovers/static/media/Hero3.180e1680.jpg"
+  },
+  {
+    "revision": "49915c7178fe04a238cc3f1430a10d69",
+    "url": "/OceanCovers/static/media/Hero6.49915c71.jpg"
+  },
+  {
+    "revision": "cc6c36ae49a6620ae4c9a8ca1be534ea",
+    "url": "/OceanCovers/static/media/Square1.cc6c36ae.jpg"
+  },
+  {
+    "revision": "b20b6db4701cd7396a4611ff952ef940",
+    "url": "/OceanCovers/static/media/Square2.b20b6db4.jpg"
+  },
+  {
+    "revision": "d335d8d2d15805ad0c3449d120c84cf7",
+    "url": "/OceanCovers/static/media/Square3.d335d8d2.jpg"
+  },
+  {
+    "revision": "fe4e7a2f40cc519bbb66aa65035eea40",
+    "url": "/OceanCovers/static/media/Square4.fe4e7a2f.jpg"
+  },
+  {
+    "revision": "037a7867270d4e0c6a4ecce65901d648",
+    "url": "/OceanCovers/static/media/Square5.037a7867.jpg"
+  },
+  {
+    "revision": "6d1e8c9f279ffdecaa2958911127fb3f",
+    "url": "/OceanCovers/static/media/SlideNew1.6d1e8c9f.jpg"
+  },
+  {
+    "revision": "7cb6415ce7bf5c2839f0b88d13c7dd1b",
+    "url": "/OceanCovers/static/media/SlideNew2.7cb6415c.jpg"
+  },
+  {
+    "revision": "c7524aaaee62a165bae85210fdbd7ec6",
+    "url": "/OceanCovers/static/media/coversOne.c7524aaa.jpg"
+  },
+  {
+    "revision": "096462f5ba573a3d53842aaa4d685001",
+    "url": "/OceanCovers/static/media/coversTwo.096462f5.jpg"
+  },
+  {
+    "revision": "4395a7952a34000350d8712ac8a2ac3b",
+    "url": "/OceanCovers/static/media/coversThree.4395a795.jpg"
+  },
+  {
+    "revision": "2814d08f3f4cb12ed32ccc235c9d67f8",
+    "url": "/OceanCovers/static/media/coversFour.2814d08f.jpg"
+  },
+  {
+    "revision": "c5208d37286a2585ea926959bc3252b8",
+    "url": "/OceanCovers/static/media/upholstery1.c5208d37.jpg"
+  },
+  {
+    "revision": "4f39191ee432b0566d969695b006fcc2",
+    "url": "/OceanCovers/static/media/upholstery2.4f39191e.jpg"
+  },
+  {
+    "revision": "34e8e0e45b89af2b20e9ca53e41e643e",
+    "url": "/OceanCovers/static/media/upholstery3.34e8e0e4.jpg"
+  },
+  {
+    "revision": "89bb0e37cf5d295361c691fba1d75644",
+    "url": "/OceanCovers/static/media/upholstery4.89bb0e37.jpg"
+  },
+  {
+    "revision": "fe33e02105185951f5ca0888dc459dbd",
+    "url": "/OceanCovers/static/media/auto1.fe33e021.jpg"
+  },
+  {
+    "revision": "5f99e6f66dd8897a3c2da89d4d0f8441",
+    "url": "/OceanCovers/static/media/auto2.5f99e6f6.jpg"
+  },
+  {
+    "revision": "f2742e6d5ea28276055304184ea5ad01",
+    "url": "/OceanCovers/static/media/carpet1.f2742e6d.jpg"
+  },
+  {
+    "revision": "25b16d38d967c62fa2ddad3820c8f422",
+    "url": "/OceanCovers/static/media/carpet2.25b16d38.jpg"
+  },
+  {
+    "revision": "13032b5158b41722dbdd78e03b39821f",
+    "url": "/OceanCovers/index.html"
+  }
+];
