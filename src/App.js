f5196fadaa70fd0fd10bea39c1b3bf5d434e1050
JackwillJudge <jack.w.judge@gmail.com>
2023-01-08 00:24:58 +1300

    react router dom fix for gh pages

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import Home from "./Contents/Home";
 import Services from "./Contents/Services";
 import Gallery from "./Contents/Gallery";
 import Contact from "./Contents/Contact";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import { BrowserRouter as Route, Routes, HashRouter } from "react-router-dom";
 function App() {
   return (
     <div>
-      <NavBar />
-      <Router>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/Services" element={<Services />} />
-            <Route exact path="/Gallery" element={<Gallery />} />
-            <Route exact path="/Contact" element={<Contact />} />
-          </Routes>
-        </div>
-      </Router>
+        <NavBar />
+        <HashRouter>     
+          <div>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/Services" element={<Services />} />
+              <Route exact path="/Gallery" element={<Gallery />} />
+              <Route exact path="/Contact" element={<Contact />} />
+            </Routes>
+          </div>
+      </HashRouter>
+ 
     </div>
   );
 }
